1946e4606393adedc6b9f25f5a84846c87e8d5b7
Gustavo <gustavo.prog40@gmail.com>
2023-03-08 19:32:16 -0300

    first

---
+++ src/controllers/urlController.ts
@@ -0,0 +1,68 @@
+import { PrismaClient } from '@prisma/client'
+import { Request, Response } from 'express'
+
+const prisma = new PrismaClient()
+
+async function registerUser(req: Request, res: Response) {
+  try {
+    const url = await prisma.biblioUrl.create({
+      data: {
+        name: req.body.name,
+        sobrenome: req.body.sobrenome,
+        area: req.body.area,
+        titulo: req.body.titulo,
+        edicao: req.body.edicao,
+        ano: req.body.ano,
+        cidade: req.body.cidade,
+      },
+    })
+    return res.status(201).send({ msg: 'url created Sucessfuly!!', url })
+  } catch (error) {
+    return res.status(400).send({ msg: 'ERROR!!', error })
+  }
+}
+
+async function getAll(req: Request, res: Response) {
+  try {
+    const data = await prisma.biblioUrl.findMany()
+
+    return res.status(200).send(data)
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+async function updateUser(req: Request, res: Response) {
+  try {
+    await prisma.biblioUrl.update({
+      where: { id: req.params.id },
+      data: {
+        name: req.body.name,
+        sobrenome: req.body.sobrenome,
+        area: req.body.area,
+        titulo: req.body.titulo,
+        edicao: req.body.edicao,
+        ano: req.body.ano,
+        cidade: req.body.cidade
+      },
+    })
+
+    return res.status(200).send({ msg: 'User Deleted!!' })
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+async function deleteUser(req: Request, res: Response) {
+  try {
+    await prisma.biblioUrl.delete({
+      where: { id: req.params.id },
+    })
+
+    return res.status(200).send({ msg: 'User Updated!!' })
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+export default { registerUser, getAll, deleteUser, updateUser }
---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from 'express'
+import urlController from './controllers/urlController'
+
+const routes = Router()
+
+routes.get('/', function (req: Request, res: Response) {
+  return res.send({ msg: ` 🌄 Api Running` })
+})
+
+routes.get('/all', urlController.getAll)
+routes.post('/register', urlController.registerUser)
+routes.put('/update/:id', urlController.updateUser)
+routes.delete('/delete/:id', urlController.deleteUser)
+
+export default routes
